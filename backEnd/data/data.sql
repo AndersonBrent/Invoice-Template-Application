dfe64bc0efbd385c82c4e3d05f5d9c0444787d9a
ShaunG-MetroState <77358928+ShaunG-MetroState@users.noreply.github.com>
2021-11-14 22:58:43 -0600

    added some test data

--- backEnd/data/data.sql
+++ backEnd/data/data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2021 at 12:42 AM
+-- Generation Time: Nov 15, 2021 at 05:57 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -29,7 +29,8 @@ USE `invoice`;
 
 INSERT INTO `clients` (`id`, `user_id`, `name`, `email`, `addr_street`, `addr_city`, `addr_state`, `phone`) VALUES
 (1, 1, 'Clint', 'clientemail@email.com', NULL, NULL, NULL, '1112223333'),
-(2, 1, 'Clint\'s Brother', 'bro@email.com', NULL, NULL, NULL, '555555555');
+(2, 1, 'Clint\'s Brother', 'bro@email.com', NULL, NULL, NULL, '555555555'),
+(3, 2, 'Guest client', 'guestclient@email.com', '123 Sesame St', 'Cityville', 'Some State', '1111111111');
 
 --
 -- Dumping data for table `invoices`
@@ -37,21 +38,24 @@ INSERT INTO `clients` (`id`, `user_id`, `name`, `email`, `addr_street`, `addr_ci
 
 INSERT INTO `invoices` (`id`, `client_id`, `user_id`, `billing_date`, `due_date`, `charge`, `is_paid`) VALUES
 (1, 2, 1, '2021-11-13', NULL, '1000.00', 0),
-(2, 1, 1, '2021-11-13', NULL, '125.56', 0);
+(2, 1, 1, '2021-11-13', NULL, '125.56', 0),
+(3, 3, 2, '2021-11-14', '2021-11-15', '50.00', 0);
 
 --
 -- Dumping data for table `items`
 --
 
 INSERT INTO `items` (`id`, `client_id`, `description`, `rate`, `quantity`, `tax`, `note`) VALUES
-(1, 1, 'thing', '19.99', 400, '0.07', 'it\'s an item');
+(1, 1, 'thing', '19.99', 400, '0.07', 'it\'s an item'),
+(2, 2, 'another piece of junk', '56.50', 3, '0.09', 'Yup');
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `phone`) VALUES
-(1, 'admin', 'pass', 'Admin Account', 'admin@test.com', '1555222333');
+(1, 'admin', 'pass', 'Admin', 'admin@email.com', '1555222333'),
+(2, 'guest', 'guest', 'Guest', 'guest@email.com', '6514888888');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
